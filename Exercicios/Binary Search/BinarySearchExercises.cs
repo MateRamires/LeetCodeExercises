8e419713dcf56bdebe6664651e535b99032f33df
MateRamires <matheushenriqueramires@gmail.com>
2025-04-08 11:07:12 -0300

    Exercicio Binary search 704 e Stack 853 e 22

---
+++ Exercicios/Binary Search/BinarySearchExercises.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicios.Binary_Search;
+
+public class BinarySearchExercises
+{
+    //Ex 704
+    public int Search(int[] nums, int target)
+    {
+        int leftP = 0, rightP = nums.Length - 1;
+
+        while (leftP <= rightP) 
+        {
+            int middle = leftP + ((rightP - leftP) / 2); //rightP - leftP seria a distancia total, dividimos por 2 e temos quantas casas temos que andar, soma essas casas que temos que andar + o inicio (que pode ser 0 ou um numero maior) e temos o meio.
+            //int middle = (leftP + rightP) / 2;
+
+            if (nums[middle] > target)
+            {
+                rightP = middle - 1;
+            }
+            else if (nums[middle] < target)
+            {
+                leftP = middle + 1;
+            }
+            else 
+            {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
--- Exercicios/Stack/StackExercises.cs
+++ Exercicios/Stack/StackExercises.cs
@@ -85,4 +85,51 @@ public class StackExercises
         return res;
     }
 
+    //Ex 853
+    public int CarFleet(int target, int[] position, int[] speed)
+    {
+        int[][] pair = new int[position.Length][];
+        for (int i = 0; i < position.Length; i++)
+        {
+            pair[i] = new int[] { position[i], speed[i] };
+        }
+        Array.Sort(pair, (a, b) => b[0].CompareTo(a[0]));
+        Stack<double> stack = new Stack<double>();
+        foreach (var p in pair) 
+        {
+            stack.Push((double)(target - p[0]) / p[1]);
+            if (stack.Count >= 2 && stack.Peek() <= stack.ElementAt(1)) 
+            {
+                stack.Pop();
+            }
+        }
+        return stack.Count;
+    }
+
+    //Ex 22
+    private void Backtrack(int openParenthesesCount, int closedParanthesesCount, int n, IList<string> res, string stack) 
+    {
+        if (openParenthesesCount == closedParanthesesCount && openParenthesesCount == n) 
+        {
+            res.Add(stack);
+            return;
+        }
+
+        if (openParenthesesCount < n)
+            Backtrack(openParenthesesCount + 1, closedParanthesesCount, n, res, stack + '(');
+
+        if (closedParanthesesCount < openParenthesesCount)
+            Backtrack(openParenthesesCount, closedParanthesesCount + 1, n, res, stack + ')');
+    }
+
+    public IList<string> GenerateParenthesis(int n)
+    {
+        IList<string> res = new List<string>();
+        string stack = "";
+        Backtrack(0, 0, n, res, stack);
+        return res;
+    }
+
+    //Ex 84
+
 }
