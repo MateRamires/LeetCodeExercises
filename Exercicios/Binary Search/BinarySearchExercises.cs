2874cb4554e1b8a832626f4852a8f26eb46b227b
MateRamires <matheushenriqueramires@gmail.com>
2025-04-17 21:10:38 -0300

    Binary Search: Exercise 4 completed with comments

--- Exercicios/Binary Search/BinarySearchExercises.cs
+++ Exercicios/Binary Search/BinarySearchExercises.cs
@@ -161,4 +161,50 @@ public class BinarySearchExercises
         }
         return -1; //Se sair do while e nao encontrar o valor, entao o valor nao existe no array.
     }
+
+    //Ex 4
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] A = nums1;
+        int[] B = nums2;
+        int total = A.Length + B.Length;
+        int half = (total + 1) / 2;
+
+        if (B.Length < A.Length) //Para esse ex ficar mais facil, nos faremos binary search apenas no array que tem menor tamanho, para isso, vamos garantir que sempre o array A tenha o menor tamanho, trocando os elementos entre ambos caso A seja maior no parametro da funcao.
+        {
+            int[] temp = A;
+            A = B;
+            B = temp;
+        }
+
+        int leftPointer = 0, rightPointer = A.Length;
+        while (leftPointer <= rightPointer)
+        {
+            int i = (leftPointer + rightPointer) / 2; //Encontramos a casa do elemento do meio do array A.
+            int j = half - i; //Encontramos quantos elementos teremos que pegar do array B.
+
+            int ALeft = 1 > 0 ? A[i - 1] : int.MinValue;
+            int Aright = i < A.Length ? A[i] : int.MaxValue;
+            int Bleft = j > 0 ? B[j - 1] : int.MinValue;
+            int Bright = j < B.Length ? B[j] : int.MaxValue;
+
+            if (ALeft <= Bright && Bleft <= Aright)
+            {
+                if (total % 2 != 0) //Se o total numero de elementos for impar.
+                {
+                    return Math.Max(ALeft, Bleft);
+                }
+                return (Math.Max(ALeft, Bleft) + Math.Min(Aright, Bright)) / 2;
+            }
+            else if (ALeft > Bright)
+            {
+                rightPointer = i - 1;
+            }
+            else
+            {
+                leftPointer = i - 1;
+            }
+        }
+        return -1;
+    }
 }
