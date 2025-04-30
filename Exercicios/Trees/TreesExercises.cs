eb9505f9382c630ca4b918c8a3decd35bb66026e
MateRamires <matheushenriqueramires@gmail.com>
2025-04-30 10:15:04 -0300

    Trees: Exercise 104 completed with three different solutions (DFS, BFS and Iteractive DFS) with comments for each one

--- Exercicios/Trees/TreesExercises.cs
+++ Exercicios/Trees/TreesExercises.cs
@@ -24,7 +24,6 @@ public class TreesExercises
         Ex543DFS(root, ref res); // Esse DFS nao vai retornar o diametro, ele vai retornar a altura de um dos lados apenas.
         return res;
     }
-
     private int Ex543DFS(TreeNode curr, ref int res) 
     {
         if (curr == null) //Toda recursao necessita de um exit, para que a recursao nao tenha a possibilidade de rodar para sempre, aqui a recursao saira caso o node atual seja nulo.
@@ -46,4 +45,58 @@ public class TreesExercises
         else //Esse else cobre todos os outros casos, por exemplo, ele cobre se um dos dois nodes for nulo, e o outro nao e cobre se o valor de um node for diferente do outro.
             return false;
     }
+
+    //Ex 104 (Esse exercicio tem tres solucoes possiveis, vou fazer as tres.)
+    public int MaxDepthDfs(TreeNode root)
+    {
+        if (root == null) //Todos os exercicios que utilizam DFS com recursao, TEM que ter um "Base case", ele eh usado para interromper as chamadas recursivas quando chegam em um determinado ponto, sem ele, ou dara um null exception ou a recursao rodara ate dar um stack overflow. Nesse caso, ele sera acionado quando chegarmos em um node que nao tem filhos, quando isso acontecer, essa funcao sera chamada passando os filhos, que nao existem, logo, sao nulos e cairam nesse if, retornando 0 e terminando a recursao.
+            return 0;
+
+        return 1 + Math.Max(MaxDepthDfs(root.left), MaxDepthDfs(root.right)); //Aqui a logica sera a seguinte, todas as vezes que essa funcao for chamada, e o node analisado nao for null ele retornara 1, e chamara os nodes filhos, os nodes filhos vao ser chamados ate acabar, retornando + 1 cada um, quando chegarmos ao fim da recursao ele vai voltando (1 + 1 + 1) e por fim teremos a soma da altura maxima entre esquerda e direita + 1 (que eh o 1 do node head), e nos dara o resultado certo. 
+    }
+    public int MaxDepthBfs(TreeNode root) //Em BFS nos vamos checando a arvore NIVEL a NIVEL, ou seja, vamos checando ela de forma horizontal, sendo assim, para resolver esse problema, basta checarmos qual o ultimo nivel, o ultimo nivel sera exatamente o tamanho da altura que precisamos
+    {
+        Queue<TreeNode> q = new Queue<TreeNode>(); //Primeiro criamos nossa queue, que eh o principio do BFS. Ela eh do tipo TreeNode logicamente.
+        if (root != null) //Checamos o edge-case, que eh o caso de o primeiro node (head) ser nulo. Se NAO for nulo, ai colocamos esse node como primeiro elemento da nossa queue.
+            q.Enqueue(root);
+
+        int level = 0; //Essa sera a variavel que ira "guardar" qual o nivel mais baixo da nossa arvore, e sera o nosso resultado.
+        while (q.Count > 0) //No processamento abaixo, nos vamos colocar e tirar nodes ate um determinado momento onde todos os nodes vao acabar, e ai essa condicao deixara de ser verdadeira.
+        {
+            int size = q.Count; //Tamanho da nossa queue. De inicio ela sempre sera 1, pois so temos apenas um root "head", mas depois ela aumentara de tamanho dependendo da quantidade de nodes em cada nivel.
+            for (int i = 0; i < size; i++) //Iremos rodar esse for para TODOS os elementos da nossa queue, pois vamos ter que adicionar a queue todos os filhos de todos os elementos atuais.
+            { 
+                TreeNode node = q.Dequeue(); //TIRAMOS o elemento da queue, e ao mesmo tempo colocamos esse elemento em uma variavel, pois vamos utilizar esse elemento para pegar os filhos dele.
+                if (node.left != null) //Nos dois ifs abaixo checamos se o elemento atual tem filhos a esquerda e a direita, e adiciona-os caso existam.
+                    q.Enqueue(node.left);
+                if (node.right != null)
+                    q.Enqueue(node.right);
+            }
+            level++; //Depois de remover todos os elementos do nivel atual, e colocar todos os elementos do proximo nivel da queue, nos aumentamos a variavel nivel, pois a partir de agora vamos analisar um nivel abaixo.
+        }
+        return level; //No fim, chegara um momento em que todos os nodes de um determinado nivel nao terao nenhum filho, quando isso acontecer, a queue nao tera mais nenhum node, e subsequentemente saira do while, sendo assim, a variavel nivel logicamente guardara o valor do ultimo nivel, que eh a nossa resposta, pois temos que achar a distancia mais profunda possivel, ou no caso, o ultimo nivel possivel.
+    }
+    public int MaxDepthDfsStack(TreeNode root)
+    {
+        Stack<Tuple<TreeNode, int>> stack = new Stack<Tuple<TreeNode, int>>(); //Criamos a stack que recebera um tuple (par de dados) de TreeNode e um inteiro, onde treenode, sera o node e o int sera o nivel onde esta esse node.
+        stack.Push(new Tuple<TreeNode, int>(root, 1)); //Primeiro colocamos o primeiro node e o seu nivel, que sera 1, pois o node head sempre estara no primeiro nivel
+        int res = 0;
+
+        while (stack.Count > 0) //Vamos rodar esse while ate nossa stack nao ter mais nenhum node.
+        {
+            Tuple<TreeNode, int> currentNode = stack.Pop(); //Primeiro tiramos o node da stack e ao mesmo tempo colocamos esse node a uma variavel, pois vamos utilizar esse node para pegar os filhos dele.
+            TreeNode node = currentNode.Item1; //Item1 do tuple eh o proprio node, pois a variavel "currentNode" nao eh um node, e sim um tuple que contem o node e o nivel.
+            int depth = currentNode.Item2; //Item2 eh o nivel do node, que vai incrementando a cada iteracao.
+
+            if (node != null) //Se o node nao for null, sendo que ele pode ser, ja que o node atual pode NAO ter filhos.
+            {
+                res = Math.Max(res, depth); //Aqui verificamos se o nivel que estamos analisando eh maior que o nivel que ja havia sido analisado anteriormente e colocamos o maior na variavel res.
+                stack.Push(new Tuple<TreeNode, int>(node.left, depth + 1)); //Por fim, para o while continuar ate acabar todos os nodes, vamos colocando sempre os filhos do node atual que esta sendo analisado, tanto o filho da esquerda quanto da direita, e como eles sao FILHOS, nos tambem colocamos nivel + 1, pois se sao filhos, entao eles estao em um nivel abaixo do node atual, subindo + 1 nivel.
+                stack.Push(new Tuple<TreeNode, int>(node.right, depth + 1));
+            }
+        }
+        return res; 
+    }
+
+    
 }
