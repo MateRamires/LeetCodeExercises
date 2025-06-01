4b630d128dbd54d67878021ea4e8bb3dbbd85d49
MateRamires <matheushenriqueramires@gmail.com>
2025-06-01 16:11:33 -0300

    Array & Hash: Exercise 1029 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise1929.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise1929
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] ans = new int[nums.Length * 2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            ans[i] = nums[i];
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            ans[nums.Length + i] = nums[i];
+        }
+
+        return ans;
+    }
+}
