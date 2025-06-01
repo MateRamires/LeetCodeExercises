808dbe481cfd3670e8ddc7fe36d601e71ada74b7
MateRamires <matheushenriqueramires@gmail.com>
2025-06-01 20:27:59 -0300

    Array & Hash: Exercise 27 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise27.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise27
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0; 
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k++] = nums[i];
+            }    
+        }
+        return k;
+    }
+}
