0a2f7cf357d157f8996c14e3e4cf5295be3ce45b
MateRamires <matheushenriqueramires@gmail.com>
2025-06-02 12:01:34 -0300

    Array & Hash: Exercise 169 completed

---
+++ Exercicios/Array & Hash/ArrayExercise169.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayExercise169
+{
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> countElements = new Dictionary<int, int>();
+        int res = 0; int biggestValue = 0;
+
+        foreach (int num in nums) 
+        {
+            if (!countElements.ContainsKey(num)) 
+            {
+                countElements[num] = 0;
+            }
+
+            countElements[num]++;
+
+            if (countElements[num] > biggestValue) 
+            {
+                res = num;
+                biggestValue = countElements[num];
+            }
+        }
+
+        return res;
+    }
+}
