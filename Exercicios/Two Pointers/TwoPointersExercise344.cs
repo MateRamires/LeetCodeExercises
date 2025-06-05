4d5645be24148cc1d76fd5471c0be895dad5e3be
MateRamires <matheushenriqueramires@gmail.com>
2025-06-05 01:48:52 -0300

    Two Pointers: Exercise 344 completed

---
+++ Exercicios/Two Pointers/TwoPointersExercise344.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise344
+{
+    public void ReverseString(char[] s)
+    {
+        int leftPointer = 0, rightPointer = s.Length - 1;
+        while (leftPointer < rightPointer) 
+        {
+            var temp = s[leftPointer];
+            s[leftPointer] = s[rightPointer];
+            s[rightPointer] = temp;
+
+            leftPointer++;
+            rightPointer--;
+        }
+    }
+}
