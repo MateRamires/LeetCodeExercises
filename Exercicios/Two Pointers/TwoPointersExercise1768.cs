35e3def6ad756579518e2692a203b2d4fcf7e46a
MateRamires <matheushenriqueramires@gmail.com>
2025-06-07 06:03:42 -0300

    Two Pointers: Exercise 1768 completed

---
+++ Exercicios/Two Pointers/TwoPointersExercise1768.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise1768
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        string result = "";
+
+        int leftPointerWord1 = 0, leftPointerWord2 = 0;
+        while (leftPointerWord1 < word1.Length || leftPointerWord2 < word2.Length) 
+        {
+            if (leftPointerWord1 < word1.Length)
+                result += word1[leftPointerWord1];
+
+            if (leftPointerWord2 < word2.Length)
+                result += word2[leftPointerWord2];
+
+            leftPointerWord1++;
+            leftPointerWord2++;
+        }
+
+        return result;
+    }
+}
