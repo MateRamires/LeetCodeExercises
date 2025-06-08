a5508ad1e21f417adbfb38323f8f58a547a24509
MateRamires <matheushenriqueramires@gmail.com>
2025-06-08 16:53:17 -0300

    Two Pointers & Sliding Window: Exercises 219 and 121 completed

---
+++ Exercicios/Sliding Window/SlidingWindowExercise219.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicios.Sliding_Window;
+
+public class SlidingWindowExercise219
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        HashSet<int> visitedNumbers = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            if (visitedNumbers.Contains(nums[i]))
+                return true;
+
+            visitedNumbers.Add(nums[i]);
+
+            if (visitedNumbers.Count > k)
+                visitedNumbers.Remove(nums[i - k]);
+        }
+
+        return false;
+    }
+}
---
+++ Exercicios/Two Pointers/TwoPointers121Redone.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointers121Redone
+{
+    public int MaxProfit(int[] prices)
+    {
+        int leftPointer = 0;
+        int rightPointer = 1;
+        int maximumProfit = 0;
+
+        while (rightPointer < prices.Length) 
+        {
+            if (prices[leftPointer] < prices[rightPointer])
+            {
+                maximumProfit = Math.Max(maximumProfit, prices[rightPointer] - prices[leftPointer]);
+            }
+            else 
+            {
+                leftPointer = rightPointer;
+            }
+            rightPointer++;
+        }
+
+        return maximumProfit;
+    }
+}
