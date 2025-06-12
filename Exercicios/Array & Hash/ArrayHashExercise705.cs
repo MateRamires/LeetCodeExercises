237fad7ddfd90b619f5d9449fe436006ed44fe69
MateRamires <matheushenriqueramires@gmail.com>
2025-06-12 12:22:42 -0300

    Array & Hash: Exercise 705 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise705.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise705
+{
+    public bool[] booleanArray;
+    public ArrayHashExercise705()
+    {
+        booleanArray = new bool[1000001];
+    }
+
+    public void Add(int key)
+    {
+        booleanArray[key] = true;
+    }
+
+    public void Remove(int key)
+    {
+        booleanArray[key] = false;
+    }
+
+    public bool Contains(int key)
+    {
+        if (booleanArray[key] == true)
+            return true;
+        else
+            return false;
+    }
+}
