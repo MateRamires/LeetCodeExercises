88742a347d4852035f17541e7afecda51a8fb659
MateRamires <matheushenriqueramires@gmail.com>
2025-06-13 14:01:21 -0300

    Two Pointers & Array: Exercise 3423 & 881 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise3423.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise3423
+{
+    public int MaxAdjacentDistance(int[] nums)
+    {
+        int res = 0;
+        res = Math.Abs(nums[0] - nums[nums.Length - 1]);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i + 1 < nums.Length)
+            {
+                res = Math.Max(res, Math.Abs(nums[i] - nums[i + 1]));
+            }
+        }
+
+        return res;
+    }
+}
---
+++ Exercicios/Two Pointers/TwoPointersExercise881.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise881
+{
+    public int NumRescueBoats(int[] people, int limit)
+    {
+        Array.Sort(people);
+
+        int currentMax = 0, leftPointer = 0, rightPointer = people.Length -1;
+        int boatQuantity = 0;
+
+        while (leftPointer <= rightPointer) 
+        { 
+            currentMax = people[leftPointer] + people[rightPointer];
+
+            if (currentMax <= limit)
+                leftPointer++;
+
+            rightPointer--;
+            boatQuantity++;
+        }
+
+        return boatQuantity;
+    }
+}
