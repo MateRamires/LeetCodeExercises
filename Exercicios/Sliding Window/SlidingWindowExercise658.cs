e6d96ebed60fd257fa2cf71608bd1cdff112a2de
MateRamires <matheushenriqueramires@gmail.com>
2025-06-14 14:34:59 -0300

    Sliding Window: Exercise 658 completed

---
+++ Exercicios/Sliding Window/SlidingWindowExercise658.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicios.Sliding_Window;
+
+public class SlidingWindowExercise658
+{
+    public IList<int> FindClosestElements(int[] arr, int k, int x)
+    {
+        int leftPointer = 0, rightPointer = arr.Length - 1;
+
+        while (rightPointer - leftPointer >= k) 
+        {
+            if (Math.Abs(x - arr[leftPointer]) <= Math.Abs(x - arr[rightPointer]))
+                rightPointer--;
+            else
+                leftPointer++;
+        }
+
+        var result = new List<int>();
+        for (int i = leftPointer; i <= rightPointer; i++) 
+        {
+            result.Add(arr[i]);
+        }
+
+        return result;
+    }
+}
