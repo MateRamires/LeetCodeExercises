1b16a103dac40fbc5f1dee24b9d2bb9eea2cab57
MateRamires <matheushenriqueramires@gmail.com>
2025-06-18 16:16:02 -0300

    Two Pointers: Exercise 283 completed with comments

---
+++ Exercicios/Two Pointers/TwoPointersExercise283.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise283
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int writePointer = 0;
+
+        for (int readPointer = 0; readPointer < nums.Length; readPointer++) 
+        {
+            if (nums[readPointer] != 0)
+            {
+                nums[writePointer] = nums[readPointer];
+                writePointer++;
+            }
+        }
+
+        while (writePointer < nums.Length) 
+        {
+            nums[writePointer++] = 0;
+        }
+    }
+}
