765c2b95da8d06ceb68e927a88935597bb1fd3ca
MateRamires <matheushenriqueramires@gmail.com>
2025-06-20 17:40:31 -0300

    Two Pointers: Exercise 26 completed

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,9 +1,4 @@
-﻿using Exercicios;
-using Exercicios.Array___Hash;
-using Exercicios.LinkedList;
-using Exercicios.Sliding_Window;
-using Exercicios.Stack;
-using Exercicios.Two_Pointers;
+﻿using Exercicios.Two_Pointers;
 
-SlidingWindowExercise1052 train = new SlidingWindowExercise1052();
-train.MaxSatisfied([1, 0, 1, 2, 1, 1, 7, 5], [0, 1, 0, 1, 0, 1, 0, 1], 3);
+TwoPointersExercise26 train = new TwoPointersExercise26();
+train.RemoveDuplicates([1, 1, 2]);
---
+++ Exercicios/Two Pointers/TwoPointersExercise26.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise26
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int writePointer = 0;
+        HashSet<int> uniqueElement = new HashSet<int>();
+        for (int readPointer = 0; readPointer < nums.Length; readPointer++) 
+        {
+            if (!uniqueElement.Contains(nums[readPointer]))
+            {
+                nums[writePointer++] = nums[readPointer];
+                uniqueElement.Add(nums[readPointer]);
+
+            }
+        }
+
+        return uniqueElement.Count;
+    }
+}
+z
