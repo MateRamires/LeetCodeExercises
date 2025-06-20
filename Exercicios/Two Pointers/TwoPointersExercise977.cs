49602b86e32992cbe4e4c20109a0f4247133e1ec
MateRamires <matheushenriqueramires@gmail.com>
2025-06-20 13:59:08 -0300

    Two Pointers: Exercise 977 completed

---
+++ Exercicios/Two Pointers/TwoPointersExercise977.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise977
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        int leftPointer = 0, rightPointer = nums.Length - 1, write = nums.Length - 1;
+        int[] res = new int[nums.Length];
+
+        while (leftPointer <= rightPointer) 
+        {
+            int sqLeft = nums[leftPointer] * nums[leftPointer];
+            int sqRight = nums[rightPointer] * nums[rightPointer];
+
+            if (sqLeft > sqRight)
+            {
+                res[write--] = sqLeft;
+                leftPointer++;
+            }
+            else
+            {
+                res[write--] = sqRight;
+                rightPointer--;
+            }
+
+        }
+        return res;
+    }
+}
