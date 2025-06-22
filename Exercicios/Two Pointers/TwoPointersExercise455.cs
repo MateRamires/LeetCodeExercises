3bda625c4eb15b9cb407b90ff174cc43d53d9dce
MateRamires <matheushenriqueramires@gmail.com>
2025-06-22 15:32:52 -0300

    TwoPointers: Exercise 455 completed

---
+++ Exercicios/Two Pointers/TwoPointersExercise455.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise455
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        Array.Sort(g);
+        Array.Sort(s);
+
+        int greedFactorPointer = 0, cookiePointer = 0, contentChildren = 0;
+
+        while (greedFactorPointer < g.Length && cookiePointer < s.Length) 
+        {
+            if (s[cookiePointer] >= g[greedFactorPointer])
+            {
+                contentChildren++;
+                cookiePointer++;
+                greedFactorPointer++;
+            }
+            else if (g[greedFactorPointer] > s[cookiePointer]) 
+            { 
+                cookiePointer++;
+            }
+        }
+
+        return contentChildren;
+    }
+}
