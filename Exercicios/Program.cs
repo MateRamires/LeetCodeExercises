5b5498bcabf84119f8efc1b17bd249ff5a14aee3
MateRamires <matheushenriqueramires@gmail.com>
2025-06-23 17:24:02 -0300

    Two Pointers: Exercise 2108 completed

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Exercicios.Two_Pointers;
 
-TwoPointersExercise2570 train = new TwoPointersExercise2570();
-train.MergeArrays([[1, 2], [2, 3], [4, 5]], [[1, 4], [3, 2], [4, 1]]);
+TwoPointersExercise2108 train = new TwoPointersExercise2108();
+train.FirstPalindrome(["abc", "car", "ada", "racecar", "cool"]);
---
+++ Exercicios/Two Pointers/TwoPointersExercise2108.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise2108
+{
+    public string FirstPalindrome(string[] words)
+    {
+        foreach (var word in words) 
+        { 
+            int leftPointer = 0, rightPointer = word.Length - 1;
+            bool isPalindrome = true;
+
+            while (leftPointer < rightPointer) 
+            {
+                if (word[leftPointer] != word[rightPointer])
+                    isPalindrome = false;
+
+                leftPointer++;
+                rightPointer--;
+            }
+
+            if (isPalindrome)
+                return word;
+        }
+
+        return "";
+    }
+}
