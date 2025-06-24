43575e8c856dde4b4c68929cea8a79da195a78ac
MateRamires <matheushenriqueramires@gmail.com>
2025-06-24 12:37:08 -0300

    Two Pointers: Exercise 905 completed

---
+++ Exercicios/Two Pointers/TwoPointersExercise905.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise905
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+        List<int> oddNumbers = new List<int>();
+        int writerPointer = 0;
+
+        for (int readPointer = 0; readPointer < nums.Length; readPointer++) 
+        {
+            if (nums[readPointer] % 2 == 0)
+            {
+                nums[writerPointer++] = nums[readPointer];
+            }
+            else 
+            {
+                oddNumbers.Add(nums[readPointer]);
+            }
+        }
+
+        foreach (int oddNum in oddNumbers)
+        {
+            nums[writerPointer++] = oddNum;
+        }
+
+        return nums;
+    }
+}
