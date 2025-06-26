e0a1dd31f20e053aa063b559b44f1834f135785f
MateRamires <matheushenriqueramires@gmail.com>
2025-06-26 13:20:33 -0300

    Two Pointers: Exercises 2109 and 2460 completed

---
+++ Exercicios/Two Pointers/TwoPointersExercise2109.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise2109
+{
+    public string AddSpaces(string s, int[] spaces)
+    {
+        var sb = new StringBuilder(s.Length + spaces.Length); //Tem quer usar stringBuilder, pois so com string vai estourar o tempo limite, ja que para cada adicao de letra a string, sera criado uma nova string, que demora muito mais. Usando string builder, ele ira realmente adicionar letra a letra, sem ter que criar uma nova string a cada adicao.
+
+        int spacesReaderPointer = 0;
+
+        for (int i = 0; i < s.Length; i++) 
+        {
+            if (spacesReaderPointer < spaces.Length && i == spaces[spacesReaderPointer]) 
+            {
+                sb.Append(' ');
+                spacesReaderPointer++;
+            }
+            sb.Append(s[i]);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Exercicios/Two Pointers/TwoPointersExercise2460.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointersExercise2460
+{
+    public int[] ApplyOperations(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++) 
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int writerPointer = 0;
+        for (int readerPointer = 0; readerPointer < nums.Length; readerPointer++) 
+        {
+            if (nums[readerPointer] != 0)
+            {
+                nums[writerPointer++] = nums[readerPointer];
+            }
+        }
+
+        while (writerPointer < nums.Length)
+        {
+            nums[writerPointer++] = 0;
+        }
+
+        return nums;
+    }
+}
