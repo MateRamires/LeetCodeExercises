3468ff417a5b67139e2464a6196e2d947400c8e2
MateRamires <matheushenriqueramires@gmail.com>
2025-06-28 16:38:27 -0300

    Array & Hash: Exercise 58 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise2486.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise2486
+{
+    public int AppendCharacters(string s, string t)
+    {
+        int substringReaderPointer = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == t[substringReaderPointer]) 
+            {
+                substringReaderPointer++;
+            }
+
+            if (substringReaderPointer == t.Length)
+                return 0;
+        }
+
+        return t.Length - substringReaderPointer;
+    }
+}
---
+++ Exercicios/Array & Hash/ArrayHashExercise58.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise58
+{
+    public int LengthOfLastWord(string s)
+    {
+        int lenghtOfCurrentWord = 0;
+        s = s.Trim();
+        for (int i = 0; i < s.Length; i++) 
+        {
+            if (s[i] == ' ') 
+            {
+                lenghtOfCurrentWord = 0;
+                continue;
+            }
+
+            lenghtOfCurrentWord++;
+        }
+
+        return lenghtOfCurrentWord;
+    }
+}
