ce9437995928f3add6e00237130f41a0e87628ff
MateRamires <matheushenriqueramires@gmail.com>
2025-06-29 22:38:18 -0300

    Sliding Window: Exercise 1456 completed

---
+++ Exercicios/Sliding Window/SlidingWindowExercise1456.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicios.Sliding_Window;
+
+public class SlidingWindowExercise1456
+{
+    public int MaxVowels(string s, int k)
+    {
+        int leftPointer = 0;
+        int highestAmmountOfVowelsInK = 0;
+        int ammountOfVowelsInCurrentWindow = 0;
+        HashSet<char> vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u'};
+        for (int rightPointer = 0; rightPointer < s.Length; rightPointer++)
+        {
+            if(vowels.Contains(s[rightPointer]))
+                ammountOfVowelsInCurrentWindow++;
+
+            if (rightPointer - leftPointer + 1 == k) 
+            {
+                highestAmmountOfVowelsInK = Math.Max(highestAmmountOfVowelsInK, ammountOfVowelsInCurrentWindow);
+
+                if (vowels.Contains(s[leftPointer]))
+                    ammountOfVowelsInCurrentWindow--;
+
+                leftPointer++;
+            }
+        }
+
+        return highestAmmountOfVowelsInK;
+    }
+}
