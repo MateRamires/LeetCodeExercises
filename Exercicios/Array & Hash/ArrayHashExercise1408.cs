c48c3d5e93ee73c75f61613c17a219ce7213a97b
MateRamires <matheushenriqueramires@gmail.com>
2025-06-30 09:59:09 -0300

    Array & Hash: Exercise 1408 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise1408.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise1408
+{
+    public IList<string> StringMatching(string[] words)
+    {
+        Array.Sort(words, (a, b) => a.Length.CompareTo(b.Length));
+        List<string> substrings = new List<string>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = i + 1; j < words.Length; j++)
+            {
+                if (words[j].Contains(words[i])) 
+                {
+                    substrings.Add(words[i]);
+                    break;
+                }
+            }
+        }
+        return substrings;
+    }
+}
