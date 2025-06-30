51e46dfc123b589045bbe32b228e65033224484b
MateRamires <matheushenriqueramires@gmail.com>
2025-06-30 09:36:00 -0300

    Two Pointers: Exercise 2161 completed

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Exercicios.Array___Hash;
 using Exercicios.Two_Pointers;
 
-ArrayHashExercise2678 train = new ArrayHashExercise2678();
-train.CountSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"]);
+TwoPointers2161 train = new TwoPointers2161();
+train.PivotArray([9, 12, 5, 10, 14, 3, 10], 10);
---
+++ Exercicios/Two Pointers/TwoPointers2161.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class TwoPointers2161
+{
+    public int[] PivotArray(int[] nums, int pivot)
+    {
+        List<int> elementsGreaterThanPivot = new List<int>();
+        int elementsEqualToPivot = 0;
+        int writerPointer = 0;
+
+        for (int readerPointer = 0; readerPointer < nums.Length; readerPointer++)
+        {
+            if (nums[readerPointer] < pivot)
+                nums[writerPointer++] = nums[readerPointer];
+            else if (nums[readerPointer] == pivot)
+                elementsEqualToPivot++;
+            else
+                elementsGreaterThanPivot.Add(nums[readerPointer]);
+        }
+
+        while (elementsEqualToPivot != 0) 
+        {
+            nums[writerPointer++] = pivot;
+            elementsEqualToPivot--;
+        }
+
+        foreach (int element in elementsGreaterThanPivot)
+        {
+            nums[writerPointer++] = element;
+        }
+
+
+        return nums;
+    }
+}
