9d4f1d50c88aaf4f08de2ce23de792b56caa77a1
MateRamires <matheushenriqueramires@gmail.com>
2025-07-03 15:38:11 -0300

    Stack: Exercise 1544 completed

---
+++ Exercicios/Stack/StackExercise1544.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Exercicios.Stack;
+
+public class StackExercise1544
+{
+    public string MakeGood(string s)
+    {
+        StringBuilder stack = new StringBuilder();
+
+        for (int i = 0; i < s.Length; i++) 
+        {
+            if (stack.Length > 0 && Math.Abs(stack[stack.Length - 1] - s[i]) == 32)
+                stack.Remove(stack.Length - 1, 1);
+            else
+                stack.Append(s[i]);
+        }
+
+        return stack.ToString();
+    }
+}
