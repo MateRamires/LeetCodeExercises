304fd4345ed039d29da8c2f296dc58bbca793a7d
MateRamires <matheushenriqueramires@gmail.com>
2025-07-03 15:46:59 -0300

    Stack: Exercise 1544 solved using stack

--- Exercicios/Stack/StackExercise1544.cs
+++ Exercicios/Stack/StackExercise1544.cs
@@ -17,5 +17,21 @@ public class StackExercise1544
         }
 
         return stack.ToString();
+
+
+        /*
+         * Stack<char> stack = new Stack<char>();
+         * 
+         * foreach(char c in s) 
+         * {
+         *      if(stack.Count > 0 && Math.Abs(stack.Peek() - c) == 32)
+         *          stack.Pop();
+         *      else
+         *          stack.Push(c);
+         * }
+         * 
+         * return new string(stack.Reverse().ToArray());
+         
+        */
     }
 }
