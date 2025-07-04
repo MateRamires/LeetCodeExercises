5048e7fc4c58a2ad444594da11fb320523c5a324
MateRamires <matheushenriqueramires@gmail.com>
2025-07-04 12:36:47 -0300

    Stack: Exercise 2390 completed

---
+++ Exercicios/Stack/StackExercise2390.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Exercicios.Stack;
+
+public class StackExercise2390
+{
+    public string RemoveStars(string s)
+    {
+        StringBuilder stack = new StringBuilder();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '*') 
+                stack.Remove(stack.Length - 1, 1);
+            else
+                stack.Append(s[i]);
+            
+        }
+
+        return stack.ToString();
+    }
+}
