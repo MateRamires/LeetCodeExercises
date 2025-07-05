86cd63c1dfb538bc9aa40dac121e4ca99921d5f3
MateRamires <matheushenriqueramires@gmail.com>
2025-07-05 12:51:28 -0300

    Stack: Added comments do exercise 496

--- Exercicios/Stack/StackExercise496.cs
+++ Exercicios/Stack/StackExercise496.cs
@@ -7,7 +7,7 @@ public class StackExercise496
         Dictionary<int, int> nextGreater = new Dictionary<int, int>();
         Stack<int> monotonic = new Stack<int>();
 
-        for (int i = nums2.Length - 1; i >= 0; i--)
+        for (int i = nums2.Length - 1; i >= 0; i--) //Base do monotonic stack: SEMPRE vamos adicionar o numero atual a stack, mas antes, precisamos rodar um while removendo todos os numeros presentes na stack que sao menores que o numero atual (ou maiores, dependendo do exercicio), se ainda restar algum numero na stack, significa que ha um valor que atende as nossas condicoes, e no ternario, iremos ver que o stack ainda tem valores, portanto colocamos o valor mais proximo do stack. Por isso usamos stack, pois estamos buscando os valores mais PROXIMOS, entao o valor do peek, eh justamente o mais proximo do numero atual que atende as condicoes
         {
             int num = nums2[i];
 
