1ca4954d6b96f8ce0fd2fb091f61b9bcafb1bd36
MateRamires <matheushenriqueramires@gmail.com>
2025-07-05 10:02:42 -0300

    Array & Hash: exercise 485 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise485.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise485
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int maximumNumberOfOnes = 0;
+        int currentNumberOfOnes = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+                currentNumberOfOnes = 0;
+            else
+                currentNumberOfOnes++;
+
+            maximumNumberOfOnes = Math.Max(maximumNumberOfOnes, currentNumberOfOnes);
+        }
+
+        return maximumNumberOfOnes;
+    }
+}
