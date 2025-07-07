b58fe3907a94da0a05920cf7ef60e56046bb8eb7
MateRamires <matheushenriqueramires@gmail.com>
2025-07-07 16:40:45 -0300

    Stack: Exercise 3174 completed

---
+++ Exercicios/Stack/StackExercise3174.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Exercicios.Stack;
+
+public class StackExercise3174
+{
+    public string ClearDigits(string s)
+    {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsDigit(s[i]))
+                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            
+            else 
+                stringBuilder.Append(s[i]);
+        }
+
+        return stringBuilder.ToString();
+    }
+}
