a867e1f19d14eff4937429828b7473b81f9aa8bf
MateRamires <matheushenriqueramires@gmail.com>
2025-07-16 11:27:24 -0300

    Linked List: Exercise 141 completed the second time

---
+++ Exercicios/LinkedList/LinkedListExercise141.cs
@@ -0,0 +1,40 @@
+﻿using Exercicios.LinkedList.Utility;
+
+namespace Exercicios.LinkedList;
+
+public class LinkedListExercise141
+{
+    public bool HasCycle(ListNode head) 
+    {
+        ListNode fastPointer = head, slowPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) 
+        {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+
+            if (fastPointer == slowPointer)
+                return true;          
+        }
+
+        return false;
+    }
+
+    public bool HasCycle2(ListNode head)
+    {
+        if (head == null || head.next == null) return false;
+
+        ListNode slowPointer = head, fastPointer = head.next;
+
+        while (fastPointer != null && fastPointer.next != null) 
+        { 
+            if(slowPointer == fastPointer)
+                return true;
+
+            slowPointer = slowPointer.next; 
+            fastPointer = fastPointer.next.next;
+        }
+
+        return false;
+    }
+}
