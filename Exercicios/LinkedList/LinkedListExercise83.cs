1d4d4ee5198366fe118642facbeccbb892dcff7e
MateRamires <matheushenriqueramires@gmail.com>
2025-07-17 17:48:32 -0300

    Linked List: Exercise 83 completed

---
+++ Exercicios/LinkedList/LinkedListExercise83.cs
@@ -0,0 +1,30 @@
+﻿using Exercicios.LinkedList.Utility;
+
+namespace Exercicios.LinkedList;
+
+public class LinkedListExercise83
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        HashSet<int> uniqueValues = new HashSet<int>();
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode previous = dummy, currentNode = head;
+        while (currentNode != null) 
+        {
+            if (!uniqueValues.Contains(currentNode.val))
+            {
+                uniqueValues.Add(currentNode.val);
+                previous = currentNode;
+            }
+            else 
+            { 
+                ListNode next = currentNode.next;
+                previous.next = next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return dummy.next;
+    }
+}
