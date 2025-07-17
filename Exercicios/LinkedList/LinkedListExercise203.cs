22b60b45fd0907b82d30def3b7ccfe820953a5a0
MateRamires <matheushenriqueramires@gmail.com>
2025-07-17 14:18:49 -0300

    Linked List: Exercise 203 completed

---
+++ Exercicios/LinkedList/LinkedListExercise203.cs
@@ -0,0 +1,27 @@
+﻿using Exercicios.LinkedList.Utility;
+
+namespace Exercicios.LinkedList;
+
+public class LinkedListExercise203
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy, currentNode = head;
+        while (currentNode != null) 
+        {
+            ListNode next = currentNode.next;
+            if (currentNode.val == val)
+            {
+                prev.next = next;
+            }
+            else 
+            { 
+                prev = currentNode;
+            }
+            currentNode = next;
+        }
+
+        return dummy.next;
+    }
+}
