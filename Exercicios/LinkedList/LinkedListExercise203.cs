bf6faf3b3f9f531b0bb9bb49eef12a504c3fb837
MateRamires <matheushenriqueramires@gmail.com>
2025-07-17 15:34:43 -0300

    Linked List: Exercise 203 new solution completed

--- Exercicios/LinkedList/LinkedListExercise203.cs
+++ Exercicios/LinkedList/LinkedListExercise203.cs
@@ -4,7 +4,7 @@ namespace Exercicios.LinkedList;
 
 public class LinkedListExercise203
 {
-    public ListNode RemoveElements(ListNode head, int val)
+    public ListNode RemoveElements(ListNode head, int val) //Without Extra Space
     {
         ListNode dummy = new ListNode(0, head);
         ListNode prev = dummy, currentNode = head;
@@ -20,6 +20,25 @@ public class LinkedListExercise203
                 prev = currentNode;
             }
             currentNode = next;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode RemoveElements2(ListNode head, int val) //With Extra Space
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode currentNode = dummy;
+
+        while (head != null) 
+        {
+            if (head.val != val) 
+            { 
+                currentNode.next = new ListNode(head.val);
+                currentNode = currentNode.next;
+            }
+
+            head = head.next;
         }
 
         return dummy.next;
