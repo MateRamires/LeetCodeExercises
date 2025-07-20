d414bb79a33f2b580fb096d6bd3c6d053506a967
MateRamires <matheushenriqueramires@gmail.com>
2025-07-20 02:33:31 -0300

    Linked List: Exercise 1669 completed

--- Exercicios/LinkedList/LinkedListExercise1669.cs
+++ Exercicios/LinkedList/LinkedListExercise1669.cs
@@ -4,5 +4,33 @@ namespace Exercicios.LinkedList;
 
 public class LinkedListExercise1669
 {
-    
+    public ListNode MergeInBetween(ListNode list1, int a, int b, ListNode list2)
+    {
+        ListNode dummy = new ListNode(0, list1);
+
+        //Apos esse for, o node de list1 sera exatamente um node antes do inicio da insercao dos novos nodes.
+        for (int i = 0; i < a - 1; i++) 
+        {
+            list1 = list1.next;
+        }
+
+        ListNode auxNode = list1;
+        for (int i = 0; i <= b - a; i++) 
+        { 
+            auxNode = auxNode.next;
+        }
+
+        ListNode dummy2 = new ListNode(0, list2);
+        while (list2.next != null) 
+        { 
+            list2 = list2.next;
+        }
+
+        if(auxNode.next != null)
+            list2.next = auxNode.next;
+
+        list1.next = dummy2.next; //Inserimos os nodes novos (list2) no lugar dos nodes a serem substituidos
+
+        return dummy.next;
+    }
 }
--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -6,10 +6,11 @@ using Exercicios.Sliding_Window;
 using Exercicios.Stack;
 using Exercicios.Two_Pointers;
 
-var ex = new LinkedListExercise876();
+var ex = new LinkedListExercise1669();
 
-var list1 = LinkedListHelpers.Build(new[] { 1, 2, 3, 4, 5 });
+var list1 = LinkedListHelpers.Build(new[] { 10, 1, 13, 6, 9, 5 });
+var list2 = LinkedListHelpers.Build(new[] { 100000, 100001, 100002 });
 
-var mergedHead = ex.MiddleNode(list1);
+var mergedHead = ex.MergeInBetween(list1, 3, 4, list2);
 
 LinkedListHelpers.Print(mergedHead);
