07434e99504ab601bb374be2e1f404a91204a122
MateRamires <matheushenriqueramires@gmail.com>
2025-07-23 08:07:06 -0300

    Linked List: Exercise 19 completed again

---
+++ Exercicios/LinkedList/LinkedListExercise19.cs
@@ -0,0 +1,27 @@
+﻿using Exercicios.LinkedList.Utility;
+
+namespace Exercicios.LinkedList;
+
+public class LinkedListExercise19
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        ListNode dummy = new ListNode(0, head);
+        ListNode leftPointer = dummy, rightPointer = head;
+
+        while (n != 0) 
+        {
+            rightPointer = rightPointer.next;
+            n--;
+        }
+
+        while (rightPointer != null) 
+        { 
+            leftPointer = leftPointer.next;
+            rightPointer = rightPointer.next;
+        }
+
+        leftPointer.next = leftPointer.next.next;
+        return dummy.next;
+    }
+}
