29eac5a227d869ecf90228763e37824a422355cd
MateRamires <matheushenriqueramires@gmail.com>
2025-07-23 07:41:07 -0300

    Array & Hash: Exercise 448 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise448.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise448
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        HashSet<int> result = new HashSet<int>();
+        for (int i = 1; i <= nums.Length; i++)
+            result.Add(i);
+
+        foreach (int num in nums)
+            result.Remove(num);
+
+        return result.ToList();
+    }
+}
