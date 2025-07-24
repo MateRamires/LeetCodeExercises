b7527f3736af2fce92eb84703e7576ca99f8392c
MateRamires <matheushenriqueramires@gmail.com>
2025-07-24 09:41:50 -0300

    Linked List: Exercise 3217 completed

---
+++ Exercicios/LinkedList/LinkedListExercise3217.cs
@@ -0,0 +1,29 @@
+﻿using Exercicios.LinkedList.Utility;
+
+namespace Exercicios.LinkedList;
+
+public class LinkedListExercise3217
+{
+    public ListNode ModifiedList(int[] nums, ListNode head)
+    {
+        HashSet<int> numsToHash = new HashSet<int>();
+        numsToHash = nums.ToHashSet();
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode previous = dummy, current = head;
+        while (current != null) 
+        {
+            if (numsToHash.Contains(current.val)) 
+            {
+                previous.next = current.next;
+            }
+            else
+            {
+                previous = previous.next;
+            }
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+}
--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -6,10 +6,10 @@ using Exercicios.Sliding_Window;
 using Exercicios.Stack;
 using Exercicios.Two_Pointers;
 
-var ex = new LinkedListExercise2487();
+var ex = new LinkedListExercise3217();
 
-var list1 = LinkedListHelpers.Build(new[] { 5, 2, 13, 3, 8 });
+var list1 = LinkedListHelpers.Build(new[] { 1, 2, 1, 2, 1, 2 });
 
-var mergedHead = ex.RemoveNodes(list1);
+var mergedHead = ex.ModifiedList([1], list1);
 
 LinkedListHelpers.Print(mergedHead);
