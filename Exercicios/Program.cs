cdb8cd9c9a2cd2076c0ca2e40b1f4ec42f6629ad
MateRamires <matheushenriqueramires@gmail.com>
2025-07-25 16:31:53 -0300

    Sliding Window: Exercise 3208 completed

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -6,10 +6,17 @@ using Exercicios.Sliding_Window;
 using Exercicios.Stack;
 using Exercicios.Two_Pointers;
 
-var ex = new LinkedListExercise3217();
+/*var ex = new LinkedListExercise3217();
 
 var list1 = LinkedListHelpers.Build(new[] { 1, 2, 1, 2, 1, 2 });
 
 var mergedHead = ex.ModifiedList([1], list1);
 
-LinkedListHelpers.Print(mergedHead);
+LinkedListHelpers.Print(mergedHead);*/
+
+
+var ex = new SlidingWindowExercise3208();
+
+var res = ex.NumberOfAlternatingGroups([0, 1, 0, 0, 1, 0, 1], 6);
+
+Console.WriteLine(res);
---
+++ Exercicios/Sliding Window/SlidingWindowExercise3208.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicios.Sliding_Window;
+
+public class SlidingWindowExercise3208
+{
+    public int NumberOfAlternatingGroups(int[] colors, int k)
+    {
+        int alternatingGroups = 0;
+        int currentWindowSize = 1;
+        for (int rightPointer = 1; rightPointer < colors.Length + k - 1; rightPointer++)
+        {
+            int trueIndex = rightPointer % colors.Length;
+            int previousIndex = (rightPointer - 1 + colors.Length) % colors.Length;
+            if (colors[trueIndex] == colors[previousIndex])
+            {
+                currentWindowSize = 1;
+            }
+            else 
+            {
+                currentWindowSize++;
+            }
+
+            if (currentWindowSize == k)
+            {
+                alternatingGroups++;
+                currentWindowSize--;
+            }
+        }
+
+        return alternatingGroups;
+    }
+}
