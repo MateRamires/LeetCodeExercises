253fc00be2e434b23fc67e0cfd22f5bae8fe2ef0
MateRamires <matheushenriqueramires@gmail.com>
2025-07-27 14:56:54 -0300

    Array & Hash: Exerc9se 1800 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise1800.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise1800
+{
+    public int MaxAscendingSum(int[] nums)
+    {
+        int highestSum = nums[0];
+        int currentSum = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                currentSum += nums[i];
+            }
+            else 
+            {
+                currentSum = nums[i];
+            }
+
+            highestSum = Math.Max(highestSum, currentSum);
+        }
+
+        return highestSum;
+    }
+}
