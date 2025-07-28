c0139e6b93eb20ebbaf8f28887b78ec6fad826ad
MateRamires <matheushenriqueramires@gmail.com>
2025-07-28 12:15:05 -0300

    Array & Hash: Exercise 2053 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise2053.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise2053
+{
+    public string KthDistinct(string[] arr, int k)
+    {
+        var freq = new Dictionary<string, int>();
+        foreach (var s in arr)
+        {
+            if (freq.ContainsKey(s))
+                freq[s]++;
+            else
+                freq[s] = 1;
+        }
+
+        foreach (var s in arr)
+        {
+            if (freq[s] == 1)
+            {
+                k--;
+                if (k == 0) return s;
+            }
+        }
+
+        return "";
+    }
+}
--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -6,12 +6,16 @@ using Exercicios.Sliding_Window;
 using Exercicios.Stack;
 using Exercicios.Two_Pointers;
 
-var ex = new LinkedListExercise1721();
+/*var ex = new LinkedListExercise1721();
 
 var list1 = LinkedListHelpers.Build(new[] { 100, 90 });
 
 var mergedHead = ex.SwapNodes(list1, 2);
 
-LinkedListHelpers.Print(mergedHead);
+LinkedListHelpers.Print(mergedHead);*/
+
+var ex = new ArrayHashExercise2053();
+
+var filhodaputadocaralho = ex.KthDistinct(["a", "b", "a", "c"], 2);
 
 
