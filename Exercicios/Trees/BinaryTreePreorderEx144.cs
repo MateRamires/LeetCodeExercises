5811092cc69e4590b834dfaa3ba5550972b41576
MateRamires <matheushenriqueramires@gmail.com>
2025-07-31 11:43:07 -0300

    Tree: Exercise 144 completed

---
+++ Exercicios/Trees/BinaryTreePreorderEx144.cs
@@ -0,0 +1,23 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class BinaryTreePreorderEx144
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        var result = new List<int>();
+        PreOrderTraversalRecursion(root, result);
+        return result;
+    }
+
+    private void PreOrderTraversalRecursion(TreeNode node, List<int> result) 
+    {
+        //Recursion base-case
+        if (node == null) return;
+
+        result.Add(node.val);
+        PreOrderTraversalRecursion(node.left, result);
+        PreOrderTraversalRecursion(node.right, result);
+    }
+}
