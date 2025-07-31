12df63492ce1d35647f111646e015d8e48363911
MateRamires <matheushenriqueramires@gmail.com>
2025-07-31 18:29:00 -0300

    Tree: Exercise 590 completed

---
+++ Exercicios/Trees/BinaryTreeNaryPostorderEx590.cs
@@ -0,0 +1,43 @@
+﻿using Exercicios.Graphs;
+
+namespace Exercicios.Trees;
+
+public class BinaryTreeNaryPostorderEx590
+{
+    public IList<int> Postorder(NodeEx590 root)
+    {
+        var result = new List<int>();
+        RecursiveTraversal(root, result);
+        return result;
+    }
+
+    public class NodeEx590
+    {
+        public int val;
+        public IList<NodeEx590> children;
+
+        public NodeEx590() { }
+
+        public NodeEx590(int _val)
+        {
+            val = _val;
+        }
+
+        public NodeEx590(int _val, IList<NodeEx590> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    private void RecursiveTraversal(NodeEx590 node, List<int> result) 
+    {
+        if (node == null) return;
+
+        foreach (var childrenNode in node.children) 
+        {
+            RecursiveTraversal(childrenNode, result);
+        }
+        result.Add(node.val);
+    }
+}
