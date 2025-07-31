0250a04f654deb4d6bc1fe2a93cf191382491c6e
MateRamires <matheushenriqueramires@gmail.com>
2025-07-31 12:02:03 -0300

    Tree: Exercise 145 completed

---
+++ Exercicios/Trees/BinaryTreePostorderEx145.cs
@@ -0,0 +1,22 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class BinaryTreePostorderEx145
+{
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
+        var result = new List<int>();
+        RecursiveTraversal(root, result);
+        return result;
+    }
+
+    private void RecursiveTraversal(TreeNode node, List<int> result) 
+    {
+        if (node == null) return;
+
+        RecursiveTraversal(node.left, result);
+        RecursiveTraversal(node.right, result);
+        result.Add(node.val);
+    }
+}
