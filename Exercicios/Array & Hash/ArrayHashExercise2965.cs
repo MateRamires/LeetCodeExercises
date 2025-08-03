729505cc8fd0fdbb4787d6dd1bffd92f7518720a
MateRamires <matheushenriqueramires@gmail.com>
2025-08-03 13:49:16 -0300

    Array & Hash: Exercise 2965 completed

---
+++ Exercicios/Array & Hash/ArrayHashExercise2965.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashExercise2965
+{
+    public int[] FindMissingAndRepeatedValues(int[][] grid)
+    {
+        var dict = new Dictionary<int, int>();
+        var range = grid.Length * grid.Length;
+
+        for (int i = 1; i <= range; i++) 
+            dict.Add(i, 0);
+
+        for (int row = 0; row < grid.Length; row++)
+        {
+            for (int i = 0; i < grid[row].Length; i++) 
+            {
+                dict[grid[row][i]]++;
+            }
+        }
+
+        var answer = new int[2];
+        foreach (var pair in dict) 
+        { 
+            if(pair.Value == 2)
+                answer[0] = pair.Key;
+
+            if (pair.Value == 0)
+                answer[1] = pair.Key;
+        }
+
+        return answer;
+    }
+}
--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -17,12 +17,17 @@ var mergedHead = ex.SwapNodes(list1, 2);
 LinkedListHelpers.Print(mergedHead);*/
 
 
-var ex = new InvertBinaryTreeEx226();
+/*var ex = new InvertBinaryTreeEx226();
 
 var tree = TreeNodeHelpers.Build(new int?[] { 4, 2, 7, 1, 3, 6, 9 });
 
 var res = ex.InvertTreeIteractive(tree);  
 
-Console.WriteLine(res);
+Console.WriteLine(res);*/
+
+
+/*var ex = new ArrayHashExercise1189();
+
+var res = ex.MaxNumberOfBalloons("loonbalxballpoon");*/
 
 
