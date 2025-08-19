2fcca0e9d5f87c3b28d61fdf6c539682a0b836e7
MateRamires <matheushenriqueramires@gmail.com>
2025-08-19 15:12:49 -0300

    Tree: Exercise 872 completed

---
+++ Exercicios/Trees/LeafSimilarTreesEx872.cs
@@ -0,0 +1,31 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class LeafSimilarTreesEx872
+{
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        List<int> leaf1 = new List<int>();
+        List<int> leaf2 = new List<int>();
+
+        DFS(root1, leaf1);
+        DFS(root2, leaf2);
+
+        return leaf1.SequenceEqual(leaf2);
+    }
+
+    public void DFS(TreeNode root, List<int> list) 
+    { 
+        if (root == null) return;
+
+        if (root.left == null && root.right == null) 
+        { 
+            list.Add(root.val);
+            return;
+        }
+
+        DFS(root.left, list);
+        DFS(root.right, list);
+    }
+}
