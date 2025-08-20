4fe14c1637483bcc6f6af0a2176ff9744a1c4dc5
MateRamires <matheushenriqueramires@gmail.com>
2025-08-20 14:42:42 -0300

    Tree: Exercise 199 completed

---
+++ Exercicios/Trees/BinaryTreeRightSideViewEx199.cs
@@ -0,0 +1,36 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class BinaryTreeRightSideViewEx199
+{
+    public IList<int> RightSideView(TreeNode root)
+    {
+        if(root == null) return new List<int>();
+
+        var result = new List<int>();
+        var queue = new Queue<TreeNode>();
+
+        queue.Enqueue(root);
+
+        while (queue.Count > 0) 
+        {
+            TreeNode rightSide = null;
+            int size = queue.Count;
+            for (int i = 0; i < size; i++) 
+            { 
+                var currentNode = queue.Dequeue();
+
+                if (i == 0) 
+                    rightSide = currentNode;
+
+                if (currentNode.right != null) queue.Enqueue(currentNode.right);
+                if (currentNode.left != null) queue.Enqueue(currentNode.left);
+            }
+
+            result.Add(rightSide.val);
+        }
+
+        return result;
+    }
+}
