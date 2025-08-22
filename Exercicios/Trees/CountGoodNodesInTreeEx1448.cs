a5104ded1cb9305ddf6752b49d2c8e72cda7d4e6
MateRamires <matheushenriqueramires@gmail.com>
2025-08-22 10:58:09 -0300

    Tree: Exercise 1448 completed

---
+++ Exercicios/Trees/CountGoodNodesInTreeEx1448.cs
@@ -0,0 +1,21 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class CountGoodNodesInTreeEx1448
+{
+    public int GoodNodes(TreeNode root)
+    {
+        return DFS(root, int.MinValue);
+    }
+
+    private int DFS(TreeNode node, int maxValue) 
+    {
+        if (node == null) return 0;
+
+        int isGood = node.val >= maxValue ? 1 : 0;
+        maxValue = Math.Max(maxValue, node.val);
+
+        return isGood + DFS(node.left, maxValue) + DFS(node.right, maxValue);
+    }
+}
