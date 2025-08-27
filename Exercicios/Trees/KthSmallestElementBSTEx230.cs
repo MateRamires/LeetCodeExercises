c31d024a08ec12b38a0e45adcceadd14a3c9a052
MateRamires <matheushenriqueramires@gmail.com>
2025-08-27 17:21:06 -0300

    Tree: exercise 230 completed

---
+++ Exercicios/Trees/KthSmallestElementBSTEx230.cs
@@ -0,0 +1,21 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class KthSmallestElementBSTEx230
+{
+    public int KthSmallest(TreeNode root, int k)
+    {
+        var list = new List<int>();
+        DFS(root, list);
+        return list[k - 1];
+    }
+
+    private void DFS(TreeNode node, List<int> list) 
+    {
+        if (node == null) return;
+        DFS(node.left, list);
+        list.Add(node.val);
+        DFS(node.right, list);
+    }
+}
