6fd70e4c4fc6dff3ac19df152124e3895d360153
MateRamires <matheushenriqueramires@gmail.com>
2025-08-28 12:36:33 -0300

    Two Pointers: Exercise 15 three sum completed

---
+++ Exercicios/Two Pointers/ThreeSumEx15.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercicios.Two_Pointers;
+
+public class ThreeSumEx15
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums);
+        var res = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue; //Se o numero atual for igual ao ultimo, pula, se nao teremos duplicatas na resposta final.
+
+            int leftPointer = i + 1, rightPointer = nums.Length -1;
+            while (leftPointer < rightPointer) 
+            { 
+                int sum = nums[i] + nums[leftPointer] + nums[rightPointer];
+                if (sum > 0)
+                {
+                    rightPointer--;
+                }
+                else if (sum < 0)
+                {
+                    leftPointer++;
+                }
+                else 
+                {
+                    res.Add(new List<int> { nums[i], nums[leftPointer], nums[rightPointer] });
+                    leftPointer++;
+                    rightPointer--;
+                    while (leftPointer < rightPointer && nums[leftPointer] == nums[leftPointer - 1]) 
+                    {
+                        leftPointer++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
