d92f0e76a54c676728e69135fdf35728ebdf1b86
MateRamires <matheushenriqueramires@gmail.com>
2025-08-29 12:32:36 -0300

    Array Hash: Ex 896 completed

---
+++ Exercicios/Array & Hash/MonotonicArrayEx896.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class MonotonicArrayEx896
+{
+    public bool IsMonotonic(int[] nums)
+    {
+        bool isIncreasingMonotonic = true;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < nums[i - 1]) 
+            {
+                isIncreasingMonotonic = false; break;
+            }
+        }
+
+        if (isIncreasingMonotonic)  return true;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+                return false;
+            
+        }
+
+        return true;
+    }
+}
