e43b9adab794c9c0bff71e37a48fc9f335649a81
MateRamires <matheushenriqueramires@gmail.com>
2025-08-31 14:51:05 -0300

    Array & Hash: Ex 238 completed again

--- Exercicios/Array & Hash/ProductArrayExceptSelfPrefixSumEx238.cs
+++ Exercicios/Array & Hash/ProductArrayExceptSelfPrefixSumEx238.cs
@@ -4,22 +4,27 @@ public class ProductArrayExceptSelfPrefixSumEx238
 {
     public int[] ProductExceptSelf(int[] nums) 
     {
-        var prefixArray = new int[nums.Length];
-        var suffixArray = new int[nums.Length];
-        var answer = new int[nums.Length];
+        int[] answer = new int[nums.Length];
+        int[] prefix = new int[nums.Length];
+        int[] suffix = new int[nums.Length];
 
-        prefixArray[0] = 1; 
-        suffixArray[nums.Length - 1] = 1;
-
+        prefix[0] = 1;
         for (int i = 1; i < nums.Length; i++) 
-            prefixArray[i] = nums[i - 1] * prefixArray[i - 1];
+        {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
 
-        for (int i = nums.Length - 2; i >= 0; i--)
-            suffixArray[i] = nums[i + 1] * suffixArray[i + 1];
+        suffix[suffix.Length - 1] = 1;
+        for (int i = suffix.Length - 2; i >= 0; i--) 
+        {
+            suffix[i] = suffix[i + 1] * nums[i + 1];    
+        }
 
-        for (int i = 0; i < nums.Length; i++)
-            answer[i] = prefixArray[i] * suffixArray[i];
-        
+        for (int i = 0; i < answer.Length; i++) 
+        {
+            answer[i] = prefix[i] * suffix[i];
+        }
+
         return answer;
     }
 }
