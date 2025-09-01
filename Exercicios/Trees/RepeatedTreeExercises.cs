fb948e6e591fe7ebd31b50e669935c46297a46ab
MateRamires <matheushenriqueramires@gmail.com>
2025-09-01 18:49:59 -0300

    Tree: Inver Tree Exercise completed

---
+++ Exercicios/Trees/RepeatedTreeExercises.cs
@@ -0,0 +1,24 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class RepeatedTreeExercises
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        DFS(root);
+        return root;
+    }
+
+    private void DFS(TreeNode root) 
+    {
+        if (root == null) return;
+
+        TreeNode tempNode = root.left;
+        root.left = root.right;
+        root.right = tempNode;
+
+        DFS(root.left);
+        DFS(root.right);
+    }
+}
