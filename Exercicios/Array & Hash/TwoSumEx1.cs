f71484817b3231a7fd2c7a0db2cc1eb03a737f2b
MateRamires <matheushenriqueramires@gmail.com>
2025-09-05 12:55:47 -0300

    Array & Hash: Exercise two sum completed

---
+++ Exercicios/Array & Hash/TwoSumEx1.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class TwoSumEx1
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var targetDifference = target - nums[i];
+
+            if (dict.ContainsKey(nums[i]))
+            {
+                return new int[] { i, dict[nums[i]] };
+            }
+
+            dict.TryAdd(targetDifference, i);
+        }
+
+        return null;
+    }
+}
