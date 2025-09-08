805b421dbdfc5068f0dd8880f4d182159dfd24ce
MateRamires <matheushenriqueramires@gmail.com>
2025-09-08 14:23:40 -0300

    Array & Hash: Exercise 128 completed

---
+++ Exercicios/Array & Hash/LongestConsecutiveSequenceEx128.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class LongestConsecutiveSequenceEx128
+{
+    public int LongestConsecutive(int[] nums)
+    {
+        var hashSet = new HashSet<int>(nums);
+
+        int longestSequence = 0;
+        foreach (var num in hashSet) 
+        {
+            if (!hashSet.Contains(num - 1)) 
+            {
+                int currentLength = 1;
+                while (hashSet.Contains(num + currentLength)) 
+                    currentLength++;
+                
+                longestSequence = Math.Max(longestSequence, currentLength);
+            }
+        }
+
+        return longestSequence;
+    }
+}
