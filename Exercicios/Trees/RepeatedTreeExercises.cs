49d474416d01d77a36a400315acfc44b3dee08a8
MateRamires <matheushenriqueramires@gmail.com>
2025-09-13 13:22:30 -0300

    Tree: exercise 100 completed

--- Exercicios/Trees/RepeatedTreeExercises.cs
+++ Exercicios/Trees/RepeatedTreeExercises.cs
@@ -30,4 +30,33 @@ public class RepeatedTreeExercises
 
         return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
     }
+
+
+    //Ex 543: Diameter of Binary Tree
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        int res = 0;
+        DFSEx543(root, ref res);
+        return res;
+    }
+
+    private int DFSEx543(TreeNode root, ref int res) 
+    {
+        if (root == null) return 0;
+        int leftSide = DFSEx543(root.left, ref res);
+        int rightSide = DFSEx543(root.right, ref res);
+        res = Math.Max(res, leftSide + rightSide);
+        return 1 + Math.Max(leftSide, rightSide);
+    }
+
+    //Ex 100: Same Tree
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == null && q == null) return true;
+
+        if (p != null && q != null && p.val == q.val) 
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        else
+            return false;
+    }
 }
