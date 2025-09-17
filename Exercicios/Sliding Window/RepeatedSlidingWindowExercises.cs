e8f1935072a84ed425ff1fbebf9f579606757071
MateRamires <matheushenriqueramires@gmail.com>
2025-09-17 10:40:39 -0300

    Sliding Window: Ex 3 completed

--- Exercicios/Sliding Window/RepeatedSlidingWindowExercises.cs
+++ Exercicios/Sliding Window/RepeatedSlidingWindowExercises.cs
@@ -4,20 +4,22 @@ public class RepeatedSlidingWindowExercises
 {
     public int LengthOfLongestSubstring(string s)
     {
+        var hashSet = new HashSet<int>();
         int leftPointer = 0;
-        var hashSet = new HashSet<char>();
-        var longestString = 0;
-        for (int rightPointer = 0; rightPointer < s.Length; rightPointer++) 
+        int longestSubstring = 0;
+
+        for (int rightPointer = 0; rightPointer < s.Length; rightPointer++)
         {
-            while (hashSet.Contains(s[rightPointer]))  
+            while (hashSet.Contains(s[rightPointer])) 
             { 
                 hashSet.Remove(s[leftPointer]);
                 leftPointer++;
             }
 
-            longestString = Math.Max(longestString, rightPointer - leftPointer + 1);
+            longestSubstring = Math.Max(longestSubstring, rightPointer - leftPointer + 1);
             hashSet.Add(s[rightPointer]);
         }
-        return longestString;
+
+        return longestSubstring;
     }
 }
