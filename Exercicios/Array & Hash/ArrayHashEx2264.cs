b6c8cd3661695827f3d031c81e95fe21f256fe2d
MateRamires <matheushenriqueramires@gmail.com>
2025-09-28 22:47:08 -0300

    stack: ex 20 completed

---
+++ Exercicios/Array & Hash/ArrayHashEx2264.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicios.Array___Hash;
+
+public class ArrayHashEx2264
+{
+    public string LargestGoodInteger(string num)
+    {
+        int currentAmmountOfSameInt = 0;
+        int maximumGoodInt = int.MinValue;
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (i > 0 && num[i] != num[i - 1]) 
+                currentAmmountOfSameInt = 0;
+
+            currentAmmountOfSameInt++;
+
+            if (currentAmmountOfSameInt == 3) 
+                maximumGoodInt = Math.Max(maximumGoodInt, int.Parse(num[i].ToString()));
+        }
+
+        return maximumGoodInt == int.MinValue ? "" : string.Concat(maximumGoodInt, maximumGoodInt, maximumGoodInt);
+    }
+}
--- Exercicios/Stack/ValidParenthesisEx20.cs
+++ Exercicios/Stack/ValidParenthesisEx20.cs
@@ -4,24 +4,23 @@ public class ValidParenthesisEx20
 {
     public bool IsValid(string s)
     {
+        //O mais recente que se abriu, eh o proximo que tem que se fechar.
         var stack = new Stack<char>();
-        var closeToOpenDict = new Dictionary<char, char> { { ')', '(' } , { '}', '{'} , { ']','[' } };
+        var closeToOpen = new Dictionary<char, char>() { { ']','[' }, { ')', '('}, { '}', '{' } };
 
         foreach (char c in s) 
         {
-            if (closeToOpenDict.ContainsKey(c)) //Se o caractere atual eh de fechamento...
+            if (closeToOpen.ContainsKey(c)) 
             {
-                if (stack.Count > 0 && stack.Peek() == closeToOpenDict[c]) //Tem que haver elementos na stack, se nao, entao estamos em um cenario onde fechamos algo sem ter aberto, retorna falso. E o outro cenario, eh que o value do elemento atual (que eh um fechamento) eh exatamente igual ao proximo elemento da stack, ou seja, a abertura.
+                if (stack.Count > 0 && stack.Peek() == closeToOpen[c])
                     stack.Pop();
                 else
                     return false;
             }
-            else //Se nao for de fechamento, eh de abertura.
-            { 
+            else
                 stack.Push(c);
-            }
         }
 
-        return stack.Count == 0 ? true : false;
+        return stack.Count == 0;
     }
 }
