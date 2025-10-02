d5b82bacdd192a90d1f7b8330f753f09809242cd
MateRamires <matheushenriqueramires@gmail.com>
2025-10-02 15:43:37 -0300

    Sliding Window: Exercise 3 completed

--- Exercicios/Sliding Window/LongestSubWithoutRepeatingCharEx3.cs
+++ Exercicios/Sliding Window/LongestSubWithoutRepeatingCharEx3.cs
@@ -1,24 +1,25 @@
 ﻿namespace Exercicios.Sliding_Window;
 
-public class LongestSubstringWithoutRepeatingCharEx3
+public class LongestSubWithoutRepeatingCharEx3
 {
     public int LengthOfLongestSubstring(string s)
     {
         int leftPointer = 0;
+        int longestSubstring = 0;
         var hashSet = new HashSet<char>();
-        int res = 0;
         for (int rightPointer = 0; rightPointer < s.Length; rightPointer++)
         {
             while (hashSet.Contains(s[rightPointer])) 
-            { 
-                hashSet.Remove(s[leftPointer]); //Nos vamos remover todas as letra, ate que a ultima removida sera a condicao do while.
+            {
+                hashSet.Remove(s[leftPointer]);
                 leftPointer++;
             }
 
+            longestSubstring = Math.Max(longestSubstring, rightPointer - leftPointer + 1);
+
             hashSet.Add(s[rightPointer]);
-            res = Math.Max(res, rightPointer - leftPointer + 1);
         }
 
-        return res;
+        return longestSubstring;
     }
 }
