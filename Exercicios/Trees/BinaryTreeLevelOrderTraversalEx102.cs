82f7f908d3fc014c9237e05e2c74a3fb985aaa7c
MateRamires <matheushenriqueramires@gmail.com>
2025-10-14 22:17:00 -0300

    Tree: Exercise 102 completed

---
+++ Exercicios/Trees/BinaryTreeLevelOrderTraversalEx102.cs
@@ -0,0 +1,34 @@
+﻿using Exercicios.Trees.Helpers;
+
+namespace Exercicios.Trees;
+
+public class BinaryTreeLevelOrderTraversalEx102
+{
+    public IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        var answer = new List<IList<int>>();
+
+        if (root == null) return answer;
+
+        var queue = new Queue<TreeNode>();
+
+        queue.Enqueue(root);
+        while (queue.Count > 0) 
+        {
+            int currentLevel = queue.Count;
+            var currentLevelList = new List<int>(); 
+            for (int i = 0; i < currentLevel; i++) 
+            { 
+                var currentNode = queue.Dequeue();
+
+                currentLevelList.Add(currentNode.val);
+
+                if (currentNode.left != null) queue.Enqueue(currentNode.left);
+                if (currentNode.right != null) queue.Enqueue(currentNode.right);
+            }
+            answer.Add(currentLevelList);
+        }
+
+        return answer;   
+    }
+}
