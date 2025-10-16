0c3b9e02d4d17930334e53107d714ae0c5141473
MateRamires <matheushenriqueramires@gmail.com>
2025-10-16 12:10:27 -0300

    Linked List: Exercise 21 completed

---
+++ Exercicios/LinkedList/MergeTwoSortedListsEx21.cs
@@ -0,0 +1,34 @@
+﻿using Exercicios.LinkedList.Utility;
+
+namespace Exercicios.LinkedList;
+
+public class MergeTwoSortedListsEx21
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode currentNode = dummy;
+
+        while (list1 != null && list2 != null) 
+        {
+            if (list1.val < list2.val) 
+            { 
+                currentNode.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+            currentNode = currentNode.next;
+        }
+
+        if (list1 != null)
+            currentNode.next = list1;
+        else
+            currentNode.next = list2;
+
+        return dummy.next;
+    }
+}
