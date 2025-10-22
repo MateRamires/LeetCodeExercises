aaf507fd40c3522f42b4e9564029074ecf50427e
MateRamires <matheushenriqueramires@gmail.com>
2025-10-22 12:49:00 -0300

    Tree: ex 572 completed

--- Exercicios/Trees/SubtreeOfAnotherTreeEx572.cs
+++ Exercicios/Trees/SubtreeOfAnotherTreeEx572.cs
@@ -7,28 +7,24 @@ public class SubtreeOfAnotherTreeEx572
     public bool IsSubtree(TreeNode root, TreeNode subRoot)
     {
         if (subRoot == null) return true;
-
         if (root == null) return false;
 
         if (SameTree(root, subRoot))
-        {
             return true;
-        }
-        else 
-        { 
-            return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
-        }
+
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
     }
 
-    private bool SameTree(TreeNode root, TreeNode subRoot) //Recursao que verifica se a arvore eh igual a partir de qualquer node.
+    public bool SameTree(TreeNode root, TreeNode subRoot) 
     {
-        if (root == null && subRoot == null)
-            return true;
+        if (root == null && subRoot == null) return true;
 
-        if (root != null && subRoot != null && root.val == subRoot.val)
-            return SameTree(root.left, subRoot.left) && SameTree(root.right, subRoot.right);
-        else 
-            return false;
-        
+        if (root != null && subRoot != null && root.val == subRoot.val) 
+        {
+            return SameTree(root.left, subRoot.left) &&
+                       SameTree(root.right, subRoot.right);
+        }
+
+        return false;
     }
 }
